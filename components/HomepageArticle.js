286e099db3829a66177438cd7895e0238dc2864d
Amelss <ameleyka@gmail.com>
2024-02-22 17:45:21 +0000

    margin top update

--- components/HomepageArticle.js
+++ components/HomepageArticle.js
@@ -37,7 +37,7 @@ export default function HomepageArticle({ article }) {
             />
 
             <div className="">
-              <h3 className="text-4xl md:text-6xl font-bold text-center my-4 md:mt-0">
+              <h3 className="text-4xl md:text-6xl font-bold text-center mb-4 md:mt-0">
                 {title}
               </h3>
               <div className="rich-text">
