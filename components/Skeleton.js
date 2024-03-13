704b2f163c10671d40cb5f9120505e8f6e2cba36
Amelss <ameleyka@gmail.com>
2024-03-13 16:00:46 +0000

    Updating skeleton

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -2,18 +2,13 @@ import React from 'react'
 
 export default function Skeleton() {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-2xl w-full p-4 bg-white shadow-md rounded-md">
-        {/* Title skeleton */}
         <div className="h-8 w-3/4 mb-4 bg-gray-300 animate-pulse rounded-md"></div>
-
-        {/* Post skeleton */}
         <div className="h-4 w-full mb-4 bg-gray-300 animate-pulse rounded-md"></div>
         <div className="h-4 w-full mb-4 bg-gray-300 animate-pulse rounded-md"></div>
         <div className="h-4 w-full mb-4 bg-gray-300 animate-pulse rounded-md"></div>
         <div className="h-4 w-full mb-4 bg-gray-300 animate-pulse rounded-md"></div>
-
-        {/* Button skeleton */}
         <div className="h-10 w-1/4 bg-gray-300 animate-pulse rounded-md"></div>
       </div>
     </div>
