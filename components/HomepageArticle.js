4d5b2f93638d73486cddc82568d1ab2b2ca7479a
Amelss <ameleyka@gmail.com>
2024-03-20 17:31:45 +0000

    Adjusting quote margins and padding

--- components/HomepageArticle.js
+++ components/HomepageArticle.js
@@ -93,7 +93,7 @@ export default function HomepageArticle({ article }) {
           </div>
         </div>
 
-        <div className="text-center text-3xl font-bold my-4">
+        <div className="text-center text-3xl font-bold  my-10 py-6 px-3 md:py-10">
           {documentToReactComponents(quote)}
         </div>
 
@@ -126,7 +126,7 @@ export default function HomepageArticle({ article }) {
           </div>
         )}
 
-        <div className="text-center text-3xl font-bold my-4">
+        <div className="text-center text-3xl font-bold my-10 py-6 px-3 md:py-10">
           {documentToReactComponents(secondQuote)}
         </div>
 
@@ -162,7 +162,7 @@ export default function HomepageArticle({ article }) {
           </div>
         )}
 
-        <div className="text-center text-3xl font-bold my-4">
+        <div className="text-center text-3xl font-bold my-10 py-6 px-3 md:py-10">
           {documentToReactComponents(thirdQuote)}
         </div>
 
@@ -198,7 +198,7 @@ export default function HomepageArticle({ article }) {
           </div>
         )}
 
-        <div className="text-center text-3xl font-bold my-4">
+        <div className="text-center text-3xl font-bold my-10 py-6 px-3 md:py-10">
           {documentToReactComponents(fourthQuote)}
         </div>
 
