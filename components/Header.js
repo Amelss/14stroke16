ce2be947a8e84e46fe3054c3ff1cfcc068be6843
Amelss <ameleyka@gmail.com>
2024-03-20 17:09:57 +0000

    making header sticky

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-      <div className='flex justify-center mt-6 mx-3 md:mx-0'>
+      <div className='flex justify-center mt-6 mx-3 md:mx-0 bg-white sticky top-0 p-3'>
          <Link href={'/'}><Image
               src={"/logo_svg.svg"}
               width={500}
