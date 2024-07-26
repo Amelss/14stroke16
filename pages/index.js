9e3ab9db52db8bf01fad08a2ffd3d5fbafc135d1
Amelss <ameleyka@gmail.com>
2024-07-26 18:50:04 +0100

    Changing order on articles published

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export async function getStaticProps() {
 
     const res = await client.getEntries({
       content_type: "article",
-      order: "sys.createdAt",
+      order: "-sys.createdAt",
     });
 
    return {
