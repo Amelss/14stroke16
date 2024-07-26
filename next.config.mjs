f7c93bd595eabe977d416242afbca53e6dfe3a57
Amelss <ameleyka@gmail.com>
2024-07-26 17:55:25 +0100

    Editing nextjs config for images

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,17 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-      protocol: 'https',
-      hostname: "images.ctfassets.net",
-      pathname: '**'
-      }
-    
-    ]
-    
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "downloads.ctfassets.net",
+        pathname: "**",
+      },
+    ],
   },
 };
-
 
 export default nextConfig;
